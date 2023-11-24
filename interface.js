686dd3fb0f741f46f68695813e13872efcf10bfe
thegove <gove.allen@gmail.com>
2023-11-24 12:59:12 -0700

    first

---
+++ interface.js
@@ -0,0 +1,28 @@
+function build_interface(){
+  // build the HTML structure to hold and display content
+  // An interface needs to have a menu, a way to invoke the settings page
+  // A table of contents
+  // landing page for when the book first opens
+
+
+}
+
+function test_menu(){
+
+}
+
+function add_item(){
+
+}
+
+function turn_page(direction, evt){
+    console.log(direction, evt)
+}
+
+function show_toc(evt){
+  console.log("showing toc", evt)
+}
+
+function show_user_menu(evt){
+  console.log("showing user menu", evt)
+}
